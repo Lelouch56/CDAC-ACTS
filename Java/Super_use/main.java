9962420a66eb794ab92c709b8cecbf4a5726ce69
Lelouch56 <145708499+Lelouch56@users.noreply.github.com>
2023-10-10 23:14:57 +0530

    Update main.java

--- Java/Super_use/main.java
+++ Java/Super_use/main.java
@@ -1,1 +1,18 @@
 
+package superkey;
+
+public class main {
+
+	public static void main(String[] args) {
+		Person p=new Person("abc","xyz");
+		Person s=new Student("pgs","aaa",56,97);
+		Person t=new Teacher("tushar","k","O",12);
+		
+		p.getDetails();
+		s.getDetails();
+		t.getDetails();
+		
+		
+	}
+
+}
