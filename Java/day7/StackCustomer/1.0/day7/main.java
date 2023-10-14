511f2ec6a07cad7145096fbd28fb352b35592cd2
Lelouch56 <145708499+Lelouch56@users.noreply.github.com>
2023-10-14 22:57:23 +0530

    Add files via upload

--- Java/day7/StackCustomer/1.0/day7/main.java
+++ Java/day7/StackCustomer/1.0/day7/main.java
@@ -1,1 +1,29 @@
+package day7;
 
+public class main {
+	
+	public static void main(String[]args) {
+	Stack s=new Stack(3);
+	Customer c1=new Customer(1,"Pgs","Pune");
+	Customer c2=new Customer(2,"ttt","VC");
+	Customer c3=new Customer(3,"vvv","Thane");
+	
+	s.push(c1);
+	s.push(c2);
+	s.push(c3);
+	s.push(c1);
+	
+	Customer a=s.pop();
+	Customer b=s.pop();
+	Customer c=s.pop();
+	
+	a.getCustomer();
+    b.getCustomer();
+	c.getCustomer();
+	
+	Customer d=s.pop();
+	//d.getCustomer();
+	}
+	
+
+}
